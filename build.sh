4b4fe2747c8074c3c671bf0a5a74fe07bafb82cb
Alfonso Saavedra "Son Link <sonlink.dourden@gmail.com>
2024-03-09 16:41:34 +0100

    Remove some files and some changes on appveyor,yml

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# Flet build script
-
-flet pack main.py --name Names.rand --product-name Names.rand --product-version "0.2.0" --copyright "Copyright (c) 2024 Son Link." --add-data names:names
